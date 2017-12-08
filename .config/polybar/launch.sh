56b0031ab91beb7637e4676f8edad81df821147a
hanss314 <24215006+hanss314@users.noreply.github.com>
2017-12-08 10:29:49 -0800

    Polybar works with multiple monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env sh
-
-# Terminate already running bar instances
 killall -q polybar
-
-# Wait until the processes have been shut down
+# Launch bar1 and bar2
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar example &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    echo $m
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
 
 
 echo "Bars launched..."
