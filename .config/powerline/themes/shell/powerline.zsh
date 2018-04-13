b64d4773de064316721aa93a8f0c0274396c8085
hanss314 <24215006+hanss314@users.noreply.github.com>
2018-04-13 10:12:05 -0700

    better

--- .config/powerline/themes/shell/powerline.zsh
+++ .config/powerline/themes/shell/powerline.zsh
@@ -164,7 +164,7 @@ _powerline_setup_prompt() {
 		local add_args_2=$add_args$new_args_2
 		add_args+=' --width=$(( ${COLUMNS:-$(_powerline_columns_fallback)} - ${ZLE_RPROMPT_INDENT:-1} ))'
 		local add_args_r2='-r .zsh '
-		typeset -g PS1='$("$POWERLINE_COMMAND" $=POWERLINE_COMMAND_ARGS shell aboveleft '$add_args')'
+		typeset -g PS1='$("$POWERLINE_COMMAND" $=POWERLINE_COMMAND_ARGS shell aboveleft '$add_args') %k%f'
         typeset -g RPS1='%D{%M:%S}[%?]'
         typeset -g RPS1=$'%F{blue}\ue0b2'"%F{white}%K{blue} %D{%M:%S} "$'%F{red}\ue0b2'"%F{black}%K{red} %? %k%f"
 #       typeset -g RPS1='$("$POWERLINE_COMMAND" $=POWERLINE_COMMAND_ARGS shell right '$add_args_r2')[%?]'
